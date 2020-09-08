4d0feae51d2faefa3007cfdf7c16087a3827700e
Silas Almeida <silasadssn@gmail.com>
2020-09-08 09:46:03 -0300

    test: tests

--- __tests__/integration/session.test.js
+++ __tests__/integration/session.test.js
@@ -5,7 +5,6 @@ const factory = require('../factories');
 const truncate = require('../utils/truncate');
 
 describe('Authentication', () => {
-
   beforeEach(async () => {
     await truncate();
   });
@@ -13,12 +12,10 @@ describe('Authentication', () => {
   it('should authenticate with valid credentials', async () => {
     const user = await factory.create('User');
 
-    const response = await request(app)
-      .post('/sessions')
-      .send({
-        email: user.email,
-        password: user.password,
-      });
+    const response = await request(app).post('/sessions').send({
+      email: user.email,
+      password: user.password,
+    });
 
     expect(response.status).toBe(200);
   });
@@ -26,12 +23,10 @@ describe('Authentication', () => {
   it('should not authenticate with invalid credentials', async () => {
     const user = await factory.create('User');
 
-    const response = await request(app)
-      .post('/sessions')
-      .send({
-        email: user.email,
-        password: '123123',
-      });
+    const response = await request(app).post('/sessions').send({
+      email: user.email,
+      password: '123123',
+    });
 
     expect(response.status).toBe(401);
   });
@@ -39,13 +34,11 @@ describe('Authentication', () => {
   it('should return jwt token when authenticated', async () => {
     const user = await factory.create('User');
 
-    const response = await request(app)
-      .post('/sessions')
-      .send({
-        name: user.name,
-        email: user.email,
-        password: user.password,
-      });
+    const response = await request(app).post('/sessions').send({
+      name: user.name,
+      email: user.email,
+      password: user.password,
+    });
 
     expect(response.body).toHaveProperty('token');
   });
@@ -55,7 +48,7 @@ describe('Authentication', () => {
 
     const response = await request(app)
       .get('/dashboard')
-      .set('Authorization', `Bearer ${user.generateToken()}`)
+      .set('Authorization', `Bearer ${user.generateToken()}`);
 
     expect(response.status).toBe(200);
   });
