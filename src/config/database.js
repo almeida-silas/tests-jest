d8c21aa1112177c4f22f343ca6eb45918b159163
Silas Almeida <silasadssn@gmail.com>
2020-09-08 09:45:14 -0300

    ci: new config

--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,5 @@
 require('dotenv').config({
-  path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env'
+  path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env',
 });
 
 module.exports = {
@@ -8,12 +8,12 @@ module.exports = {
   password: process.env.PASS,
   database: process.env.DB_NAME,
   dialect: process.env.DB_DIALECT || 'postgres',
-  storage: './__tests__/database.sqlite',
+  storage: './__tests__/test.sqlite',
   logging: false,
 
   define: {
     timestamps: true,
     underscored: true,
     underscoredAll: true,
-  }
+  },
 };
